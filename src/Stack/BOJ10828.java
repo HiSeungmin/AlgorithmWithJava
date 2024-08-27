ebad9f4affc268eb67070dc967f17bb09d4381a7
HiSeungmin <osminosmin7180@gmail.com>
2024-08-27 22:00:33 +0900

    stack

---
+++ src/Stack/BOJ10828.java
@@ -0,0 +1,53 @@
+package Stack;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ10828 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Integer> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String s = st.nextToken();
+
+            switch (s) {
+                case "push":
+                    stack.push(Integer.parseInt(st.nextToken()));
+                    break;
+                case "pop":
+                    if (stack.isEmpty()) {
+                        sb.append(-1 + "\n");
+                    } else {
+                        sb.append(stack.pop() + "\n");
+                    }
+                    break;
+                case "size":
+                    sb.append(stack.size() + "\n");
+                    break;
+                case "empty":
+                    if (stack.isEmpty()) {
+                        sb.append(1 + "\n");
+                    } else {
+                        sb.append(0 + "\n");
+                    }
+                    break;
+                case "top":
+                    if (stack.isEmpty()) {
+                        sb.append(-1 + "\n");
+                    } else {
+                        sb.append(stack.peek() + "\n");
+                    }
+                    break;
+            }
+
+        }
+
+        System.out.print(sb);
+    }
+
+}
